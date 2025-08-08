dadb4b0bed049e5a60764b4e5faed5ff4e512cc0
Bao <Bao@example.com>
2025-08-08 15:44:07 -0400

    ServiceSummary: update animated keywords

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -35,30 +35,27 @@ const ServiceSummary = () => {
   });
   return (
     <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
-      <div id="title-service-1">
-        <p>Architucture</p>
-      </div>
-      <div
-        id="title-service-2"
-        className="flex items-center justify-center gap-3 translate-x-16"
-      >
-        <p className="font-normal">Development</p>
-        <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p>Deployment</p>
-      </div>
-      <div
-        id="title-service-3"
-        className="flex items-center justify-center gap-3 -translate-x-48"
-      >
-        <p>APIs</p>
-        <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p className="italic">Frontends</p>
-        <div className="w-10 h-1 md:w-32 bg-gold" />
-        <p>Scalability</p>
-      </div>
-      <div id="title-service-4" className="translate-x-48">
-        <p>Databases</p>
-      </div>
+  <div id="title-service-1">
+  <p>Architecture</p>
+</div>
+
+<div id="title-service-2" className="flex items-center justify-center gap-3 translate-x-16">
+  <p className="font-normal">Automation</p>
+  <div className="w-10 h-1 md:w-32 bg-gold" />
+  <p>Deployment</p>
+</div>
+
+<div id="title-service-3" className="flex items-center justify-center gap-3 -translate-x-48">
+  <p>APIs</p>
+  <div className="w-10 h-1 md:w-32 bg-gold" />
+  <p className="italic">CI/CD</p>
+  <div className="w-10 h-1 md:w-32 bg-gold" />
+  <p>Scalability</p>
+</div>
+
+<div id="title-service-4" className="translate-x-48">
+  <p>Networking</p>
+</div>
     </section>
   );
 };
